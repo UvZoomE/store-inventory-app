b4d29fd809f116de456ef60559187879b5b4b9d7
Uvaldo Zumaya <uvaldo.zumaya@spaceforce.mil>
2022-09-23 21:26:36 -0600

    added node engine

--- backend/index.js
+++ backend/index.js
@@ -2,22 +2,22 @@ const express = require('express')
 const cors = require("cors")
 const path = require("path");
 const app = express()
+const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
+const productsRouter = require('./controllers/products')
 
 const PORT = process.env.PORT || 3001 
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("build"));
-}
+app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
+app.use('/api/products', productsRouter)
 
 if (process.env.NODE_ENV === "production") {
+  app.use(express.static("build"));
   app.use("*", (req, res) => {
     res.sendFile(path.join(__dirname, "./build/index.html"));
   });
 }
-
-const usersRouter = require('./controllers/users')
-const loginRouter = require('./controllers/login')
-const productsRouter = require('./controllers/products')
 
 const corsOptions ={
   origin:'http://localhost:3000', 
@@ -27,10 +27,6 @@ const corsOptions ={
 
 app.use(express.json())
 app.use(cors(corsOptions))
-
-app.use('/api/users', usersRouter)
-app.use('/api/login', loginRouter)
-app.use('/api/products', productsRouter)
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
