f32402add534b649335b558b31eb888110a55c75
Uvaldo Zumaya <uvaldo.zumaya@spaceforce.mil>
2022-09-23 20:56:08 -0600

    added node engine

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,10 @@ const app = express()
 const PORT = process.env.PORT || 3001 
 
 if (process.env.NODE_ENV === "production") {
+  app.use(express.static("build"));
+}
+
+if (process.env.NODE_ENV === "production") {
   app.use("*", (req, res) => {
     res.sendFile(path.join(__dirname, "./build/index.html"));
   });
